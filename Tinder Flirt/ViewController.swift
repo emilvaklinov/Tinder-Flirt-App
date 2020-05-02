26616f60a9e30e1765191a3ec6295cda3309d404
emil vaklinov <emil_vaklinov@yahoo.com>
2020-05-02 12:27:42 +0100

    Initial Commit

---
+++ Tinder Flirt/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tinder Flirt
+//
+//  Created by Emil Vaklinov on 02/05/2020.
+//  Copyright © 2020 Emil Vaklinov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
