1e87d414d359db76889e9fcc6dd71c6283c2d115
emil vaklinov <emil_vaklinov@yahoo.com>
2020-05-02 19:58:47 +0100

    login completed

--- Tinder Flirt/LogInViewController.swift
+++ Tinder Flirt/LogInViewController.swift
@@ -7,23 +7,77 @@
 //
 
 import UIKit
-
+import Parse
 class LogInViewController: UIViewController {
 
+    @IBOutlet weak var errorLabel: UILabel!
+    
     @IBOutlet weak var usernameTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
-    
-    @IBOutlet weak var LogInSignUpButton: UIButton!
+    @IBOutlet weak var logInSignUpButton: UIButton!
     @IBOutlet weak var changeLogInSignUpButton: UIButton!
     
+    var signUpMode = true
+    
     @IBAction func logInSignUpTapped(_ sender: Any) {
+        
+        if signUpMode {
+            let user = PFUser()
+            user.username = usernameTextField.text
+            user.password = passwordTextField.text
+            
+            user.signUpInBackground(block: { (success, error) in
+                if error != nil {
+                    var errorMessage = "Sign up Failed - Try again!"
+                    if let newError = error as NSError? {
+                        if let detailedError = newError.userInfo["error"] as? String {
+                            errorMessage = detailedError
+                        }
+                    }
+                    self.errorLabel.isHidden = false
+                    self.errorLabel.text = errorMessage
+                } else {
+                    print("Sign up Successful")
+                }
+            })
+            
+        } else {
+            if let username = usernameTextField.text {
+                if let password = passwordTextField.text {
+                    PFUser.logInWithUsername(inBackground: username, password: password, block: { (user, error) in
+                        if error != nil {
+                            var errorMessage = "Login Failed - Try again!"
+                            if let newError = error as NSError? {
+                                if let detailedError = newError.userInfo["error"] as? String {
+                                    errorMessage = detailedError
+                                }
+                            }
+                            self.errorLabel.isHidden = false
+                            self.errorLabel.text = errorMessage
+                        } else {
+                            print("Login Successful")
+                        }
+                    })
+                }
+            }
+        }
     }
     
     @IBAction func changeLogInSignUpTapped(_ sender: Any) {
+        // Creating shafle of the buttons
+        if signUpMode {
+            logInSignUpButton.setTitle("Log In", for: .normal)
+            changeLogInSignUpButton.setTitle("Sign Up", for: .normal)
+            signUpMode = false
+        } else {
+            logInSignUpButton.setTitle("Sign Up", for: .normal)
+            changeLogInSignUpButton.setTitle("Log In", for: .normal)
+            signUpMode = true
+        }
     }
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        errorLabel.isHidden = true
         // Do any additional setup after loading the view.
     }
     
