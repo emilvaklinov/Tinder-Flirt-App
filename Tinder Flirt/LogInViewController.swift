0dd18e7be7edfa08d987afcc7eafdf920e4c2779
emil vaklinov <emil_vaklinov@yahoo.com>
2020-05-02 15:08:30 +0100

    Log In view controller created

---
+++ Tinder Flirt/LogInViewController.swift
@@ -0,0 +1,32 @@
+//
+//  LogInViewController.swift
+//  Tinder Flirt
+//
+//  Created by Emil Vaklinov on 02/05/2020.
+//  Copyright © 2020 Emil Vaklinov. All rights reserved.
+//
+
+import UIKit
+
+class LogInViewController: UIViewController {
+
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBOutlet weak var LogInSignUpButton: UIButton!
+    @IBOutlet weak var changeLogInSignUpButton: UIButton!
+    
+    @IBAction func logInSignUpTapped(_ sender: Any) {
+    }
+    
+    @IBAction func changeLogInSignUpTapped(_ sender: Any) {
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
