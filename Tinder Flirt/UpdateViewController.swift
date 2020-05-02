781ba2e48cc0e7bfb21109c4c937c278ec78c136
emil vaklinov <emil_vaklinov@yahoo.com>
2020-05-02 20:26:37 +0100

    Update View Controller created

---
+++ Tinder Flirt/UpdateViewController.swift
@@ -0,0 +1,30 @@
+//
+//  UpdateViewController.swift
+//  Tinder Flirt
+//
+//  Created by Emil Vaklinov on 02/05/2020.
+//  Copyright © 2020 Emil Vaklinov. All rights reserved.
+//
+
+import UIKit
+
+class UpdateViewController: UIViewController {
+    @IBOutlet weak var profileImageView: UIImageView!
+    
+    @IBOutlet weak var userGenderSwitch: UISwitch!
+    @IBOutlet weak var interestedGenderSwitch: UISwitch!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func updateImageTapped(_ sender: Any) {
+    }
+    
+    @IBAction func updateTapped(_ sender: Any) {
+    }
+    
+}
